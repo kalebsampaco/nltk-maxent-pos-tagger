f879dfc2456996d2e9c28cda66541e4bb9e9b7a1
Caleb Saenz <38933379+kalebsampaco@users.noreply.github.com>
2020-10-19 10:50:35 -0500

    Update mxpost.py

--- mxpost.py
+++ mxpost.py
@@ -417,11 +417,11 @@ def demo(corpus, num_sents):
     train_sents, test_sents = tagged_sents[size:], tagged_sents[:size]
     maxent_tagger = MaxentPosTagger()
     maxent_tagger.train(train_sents)
-    print "tagger accuracy (test %i sentences, after training %i):" % \
+    #print ("tagger accuracy (test %i sentences, after training %i):" % \
         (size, (num_sents - size)), maxent_tagger.evaluate(test_sents)
     print "\n\n"
-    print "classify unseen sentence: ", maxent_tagger.tag(["This", "is", "so",
-        "slow", "!"])
+    print ("classify unseen sentence: ", maxent_tagger.tag(["This", "is", "so",
+        "slow", "!"]))
     print "\n\n"
     print "show the 10 most informative features:"
     print maxent_tagger.classifier.show_most_informative_features(10)
